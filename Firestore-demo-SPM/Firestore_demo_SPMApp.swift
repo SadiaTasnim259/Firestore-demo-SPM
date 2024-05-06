2f990190aa286b4cccb6c37de2759a4dc6434c6f
Sadia Tasnim <tasnimsadia259@gmail.com>
2024-05-06 11:46:50 +0600

    Initial Commit

---
+++ Firestore-demo-SPM/Firestore_demo_SPMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firestore_demo_SPMApp.swift
+//  Firestore-demo-SPM
+//
+//  Created by Sadia on 6/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Firestore_demo_SPMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
